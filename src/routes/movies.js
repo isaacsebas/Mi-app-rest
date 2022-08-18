0f15ae2df17f5ee01cb53a672e0d1193e9eb1195
isaacsebas <107631482+isaacsebas@users.noreply.github.com>
2022-08-18 08:31:32 -0500

    taller
    taller

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = new Router();
-const movies =require('../example.json');
+const movies = require('../example.json');
 
 
 
@@ -8,4 +8,32 @@ router.get('/', (req, res) => {
     res.json(movies);
 
 });
-module.exports=router;
+
+router.post('/', (req, res) => {
+
+    //Creacion de id automaticamente
+    const id = movies.length + 1;
+    const newMovie = { ...req.body, id };
+    //console.log(req.body);
+
+    //res.send('received');
+    const { title, director, year, raiting } = req.body;
+    if (id && title && director && year && raiting) {
+        movies.push(newMovie);
+        res.json(movies);
+        res.send('Se ha guardado correctamente la pelicula');
+   
+    }else if(!title){
+          res.send('Falta ingresar el titulo');
+    }else if(!director){
+        res.send('Falta ingresar el nombre del director');
+    }else if(!year){
+        res.send('Falta ingresar el año');
+    }else if(!raiting){
+        res.send('Falta ingresar el raiting');
+    }
+
+    
+
+});
+module.exports = router;
